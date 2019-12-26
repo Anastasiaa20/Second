280ee661f5078baf965732eae7ea5c916c93830c
Anastasiya <Anastasiaa20@users.noreply.github.com>
2019-12-26 20:22:53 +0300

    Update Secondfile.php

--- Secondfile.php
+++ Secondfile.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        echo "";
+    $arr = array("somearray" => array(6 => 5, 13 => 9, "a" => 42));
+
+    echo $arr["somearray"][6];  //5
+    echo $arr["somearray"][13];  //9
+    echo $arr["somearray"]["a"];  //42
     ?>
 </body>
+
 </html>
