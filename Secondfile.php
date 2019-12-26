879be398c5c7c6a1714a0302dac0a71852c1cf5a
Anastasiya <Anastasiaa20@users.noreply.github.com>
2019-12-26 21:14:53 +0300

    Update Secondfile.php

--- Secondfile.php
+++ Secondfile.php
@@ -92,6 +92,18 @@
     } else {
         echo "is a smaller than b";
     }
+
+    echo "<br>";
+
+    if ($a == 5):
+        echo "a equals 5";
+        echo "...";
+    elseif ($a ==6):
+        echo "a equals 6";
+        echo "!!!";
+    else:
+        echo "a is neither 5 nor 6";
+    endif;
     ?>
 
 
