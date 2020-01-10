4982f293563d3911065e24b07d900a53dc490ba7
Anastasiya <Anastasiaa20@users.noreply.github.com>
2020-01-10 21:20:59 +0300

    1

--- file.php
+++ file.php
@@ -176,18 +176,77 @@
 
     <?php
     $i = 1;
-    switch ($i){
+    switch ($i) {
         case 4:
             echo 4;
-        break;
+            break;
         case 5:
             echo 5;
-        break;
+            break;
         default:
             echo "\$i=$i";
     }
     ?>
 
+    <br>
+
+    <?php
+    $i = 11;
+    switch ($i) {
+        case 0:
+            echo "ноль";
+            break;
+        case 1:
+            echo "один";
+            break;
+        case 2:
+            echo "два";
+            break;
+        case 3:
+            echo "три";
+            break;
+        case 4:
+            echo "четыре";
+            break;
+        case 5:
+            echo "пять";
+            break;
+        case 6:
+            echo "шесть";
+            break;
+        case 7:
+            echo "семь";
+            break;
+        case 8:
+            echo "восемь";
+            break;
+        case 9:
+            echo "девять";
+            break;
+        default:
+            echo "нет такой цифры";
+    }
+    ?>
+
+    <br>
+
+    <?php
+    $s = 10;
+    $k = 12;
+    $n = 12;
+    ?>
+
+    <!-- HOMEWORK -->
+    <?php
+    $s = 10;
+    $k = 12;
+    $n = 1;
+    do {
+        $b = $s * ($k%12%10);
+        $s = $s + $b;
+    } while ($n <= 12);
+    echo $s;
+    ?>
 </body>
 
 </html>
