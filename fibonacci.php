e3f7530ace3236922ea46e7d8ed0983179ace001
Anastasiya <Anastasiaa20@users.noreply.github.com>
2020-01-21 18:43:59 +0300

    Update fibonacci.php

--- fibonacci.php
+++ fibonacci.php
@@ -19,65 +19,10 @@
             return fib($n - 1) + fib($n - 2);
         }
     }
-    echo fib(1);
-    echo ", ";
-    echo fib(2);
-    echo ", ";
-    echo fib(3);
-    echo ", ";
-    echo fib(4);
-    echo ", ";
-    echo fib(5);
-    echo ", ";
-    echo fib(6);
-    echo ", ";
-    echo fib(7);
-    echo "... ";
-    ?>
-
-    <br>
-
-    <?php
-    // function f($n)
-    // {
-    //     if ($n > -1 & $n < 3) {
-    //         return 1;
-    //     } else {
-    //         if ($n > 3) {
-    //             return f($n - 1) + f($n - 2);
-    //         } else {
-    //             return (pow((-1), abs($n) + 1) * f(abs($n)));
-    //         }
-    //     }
-    // }
-    // echo f(1);
-    // echo ", ";
-    // echo f(2);
-    // echo ", ";
-    // echo f(3);
-    // echo ", ";
-    // echo f(4);
-    // echo ", ";
-    // echo f(5);
-    // echo ", ";
-    // echo f(6);
-    // echo ", ";
-    // echo f(7);
-    // echo ", ";
-    // echo f(-1);
-    // echo ", ";
-    // echo f(-2);
-    // echo ", ";
-    // echo f(-3);
-    // echo ", ";
-    // echo f(-4);
-    // echo ", ";
-    // echo f(-5);
-    // echo ", ";
-    // echo f(-6);
-    // echo ", ";
-    // echo f(-7);
-    // echo "... ";
+    for ($n = 1; $n <= 10; $n++) {
+        echo fib($n) . ", ";
+    }
+    echo "..."
     ?>
 
 </body>
